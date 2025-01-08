a64cf2fa69c8efc50ccbbfbb4cba0dde07726cf3
Graham Hill <49126644+GrahamCHill@users.noreply.github.com>
2025-01-08 02:11:13 +0200

    Adjusted Loop

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        dt = clock.tick(60) / 1000.0
         pygame.display.flip()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
