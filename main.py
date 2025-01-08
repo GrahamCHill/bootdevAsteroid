f8102edab0386e4884695a6223289b7da72ce0fb
Graham Hill <49126644+GrahamCHill@users.noreply.github.com>
2025-01-08 02:02:11 +0200

    Constants added

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import constants
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen Width: {constants.SCREEN_WIDTH}")
+    print(f"Screen Height: {constants.SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
