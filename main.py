9372efe9547e7d7d5aca11338152295beee14dbf
Graham Hill <49126644+GrahamCHill@users.noreply.github.com>
2025-01-08 02:23:41 +0200

    Initial Controller

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         dt = clock.tick(60) / 1000.0
         pygame.display.flip()
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame.draw
+from pygame.transform import rotate
 
 import circleshape
 import constants
@@ -22,3 +23,15 @@ class Player(circleshape.CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+
+    def rotate(self, dt):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
+
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
